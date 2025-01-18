5264d1b3fec5239832b1ab2dfe857a0423494a63
Leshik1305 <realleshik1305@gmail.com>
2025-01-18 21:06:07 +0300

    created

--- api/models.py
+++ api/models.py
@@ -14,7 +14,13 @@ class Room(models.Model):
     num = models.PositiveIntegerField()
     hotel = models.ForeignKey(Hotel, related_name="rooms", on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"{self.hotel.name}. Room num: {self.num}"
+
 
 class Booking(models.Model):
     room = models.ForeignKey(Room, related_name="bookings", on_delete=models.CASCADE)
     user = models.ForeignKey(ApiUser, related_name="bookings", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.username}; {self.room.hotel.name}; {self.room.num}"
